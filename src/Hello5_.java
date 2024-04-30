739f8bc8bd2b316adfa8e2d36d06435dc5129663
SuperStick0141 <toitoom1030@docomo.ne.jp>
2024-04-30 14:36:35 +0900

    ss

--- src/Hello5_.java
+++ src/Hello5_.java
@@ -2,7 +2,7 @@ public class Hello5_ {
 
 	public static void main(String[] args) { //メソッド(Cでは関数と呼んだ)
 
-		System.out.println("Hello5! \"もう木村君でいいや\" \n ");
+		System.out.println("Hello5! \"    もう木村君でいいや\" \n ");
 		System.out.print("A");
 		System.out.print("B");
 		System.out.print("C");
