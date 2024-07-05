8cc6227b51d7c56f188d88f45c722b47b94550d2
SuperStick0141 <toitoom1030@docomo.ne.jp>
2024-07-05 11:46:53 +0900

    1

--- make/src/ChatServer.java
+++ make/src/ChatServer.java
@@ -73,25 +73,32 @@ public class ChatServer
             synchronized (ChatServer.class) {NextCount = 0;}
             GmSubMessage("[GM] あなたのステータスを変更します","・プレイヤー"+(PlNum+1)+"の番です",PlNum);
             GmOneMessage("[GM] まずはスキルを選択します。スキル内容は以下。\n"+
-            "スキル①:以下同じような形式で羅列\n"+
+            "スキル①:頑強 (プレイヤーの最大体力+2)\n"+
+            "スキル②:勇猛 (プレイヤーの攻撃力+2)\n"+
+            "スキル③:解体 (プレイヤーのお肉獲得量+1)\n"+
+            "スキル④:俊足 (逃走が確実に成功)\n"+
+            "スキル⑤:再生 (休憩の回復量+1)\n"+
+            "スキル⑥:鉄壁 (休憩の回復量+1)\n"+
+            "スキル⑦:万有 (休憩の回復量+1)\n"+
+            "スキル⑧:暴食 (休憩の回復量+1)\n"+
             "\nこれらから選んでください。数字1~8で回答",PlNum);
             client.CanMove=true; 
             synchronized (ChatServer.class) {while(NextCount==0){try{ChatServer.class.wait();}catch(InterruptedException e){}}}
             GmOneMessage("[GM] もう一つです。",PlNum);
             synchronized (ChatServer.class) {while(NextCount==1){try{ChatServer.class.wait();}catch(InterruptedException e){}}}
             GmOneMessage("[GM] 完了です。",PlNum);
-            client.setHP(10);
+            client.setHP();
             client.CanMove=false; PlNum++;
         }
             GmAllMessage("[GM] 全員決まったのでターンスタートです。");
             Game(1,1);
         }
         else if(Phase==1)
-        {
+        {//戦闘と休憩
             boolean GameFinish=true;
             for(ClientHandler client : clients){if(client.getHP()<=0){GameFinish=false; Game(2,0);}}
             if(GameFinish)
-            {
+            {//死人が居なければ一日を始める
             GmAllMessage("\n\n[GM] 一日が始まります。"+Turn+"日目です。");
             if(true){/* 環境を左右する時はここ */GmAllMessage("\n\n[GM] 今日は晴天です。");}
             GmAllMessage("[GM] 各プレイヤーは食料を調達するか、他の作業をするか選択できます。");
@@ -99,13 +106,14 @@ public class ChatServer
             {//各プレイヤーの行動
                 for(ClientHandler Checkclient : clients){if(Checkclient.getHP()<=0){GameFinish=false; Game(2,0);}}
                 if(GameFinish)
-                {
+                {//死人が居なければ行動を行う
                 synchronized (ChatServer.class) {NextCount = 0;}
                 GmSubMessage("\n[GM] あなたの行動を選択してください","・プレイヤー"+(PlNum+1)+"の番です",PlNum);
-                GmOneMessage("獲物を探す場合は[1]、休憩は[2]です\n",PlNum);
+                GmOneMessage("獲物を探す場合は[1]、休憩は[2]です",PlNum);
                 client.CanMove=true; 
                 synchronized (ChatServer.class) {while(NextCount==0){try{ChatServer.class.wait();}catch(InterruptedException e){}}}
-                if(NextCount==1){
+                if(NextCount==1)
+                {//戦闘開始
                     GmOneMessage("獲物を探します\n",PlNum);
                     int Enemy =random.nextInt(3);
                     String EnemyName="ウサギ";
@@ -115,35 +123,44 @@ public class ChatServer
                     else if(Enemy==3){EnemyName="ウサギD";}
                     GmOneMessage("...\n"+EnemyName+"に遭遇しました！",PlNum);
                     for(int HP=EnemyStates[Enemy][0];HP>0;)
-                    {
+                    {//獲物の体力を削るまで戦闘　敗北時にループ解除
                         synchronized (ChatServer.class) {NextCount = 1;}
                         if(client.getHP()<=0){GmSubMessage(EnemyName+"に敗北した...","・プレイヤー"+(PlNum+1)+"が"+EnemyName+"に敗北!",PlNum); break;}
                         GmOneMessage("攻撃(1)か撤退(2)か選んでください",PlNum);
                         synchronized (ChatServer.class) {while(NextCount==1){try{ChatServer.class.wait();}catch(InterruptedException e){}}}
                         if(NextCount==11)
-                        {
+                        {//攻撃→撃破時に終了、失敗で相手の攻撃
                             GmOneMessage("攻撃を行います",PlNum);
-                            int Damage =random.nextInt(3);
+                            int MaxDamage=3;
+                            if(client.retSkillA()==2||client.retSkillB()==2){MaxDamage+=2;}                            
+                            int Damage =random.nextInt(MaxDamage);
+                            if(client.retSkillA()==7||client.retSkillB()==7){Damage+=2;}
                             HP-=Damage;
                             GmOneMessage(EnemyName+"に"+Damage+"のダメージ!!  残り"+HP,PlNum);
                             if(HP<=0)
-                            {
+                            {//撃破。戦闘終了。肉取得。
                                 GmSubMessage(EnemyName+"を撃破した！","・プレイヤー"+(PlNum+1)+"が"+EnemyName+"を撃破!",PlNum);
-                                client.getMeat(EnemyStates[Enemy][2]);
-                                GmOneMessage("お肉を"+EnemyStates[Enemy][2]+"つ獲得",PlNum);
+                                int PlusMeat =0;
+                                if(client.retSkillA()==3||client.retSkillB()==3){PlusMeat=1;}
+                                client.getMeat(EnemyStates[Enemy][2]+PlusMeat);
+                                GmOneMessage("お肉を"+EnemyStates[Enemy][2]+PlusMeat+"つ獲得",PlNum);
                                 break;
                             }
                             GmOneMessage("相手が攻撃を行います",PlNum);
-                            client.getDamage(EnemyStates[Enemy][1]);
+                            int ThatDamage=EnemyStates[Enemy][1];
+                            if(client.retSkillA()==6||client.retSkillB()==6){ThatDamage-=1;}
+                            if(client.retSkillA()==7||client.retSkillB()==7){ThatDamage+=2;}
+                            client.getDamage(ThatDamage);
                             GmOneMessage(EnemyStates[Enemy][1]+"のダメージ!!\n現在の体力は"+client.getHP()+"です。",PlNum);
                         }
                         else if(NextCount==12)
-                        {
+                        {//逃亡→成功したらループ解除。得る物無。失敗時に相手の攻撃
                             GmOneMessage("逃亡します",PlNum);
                             int Ran=random.nextInt(1);
+                            if(client.retSkillA()==4||client.retSkillB()==4){Ran=0;}
                             if(Ran==0){HP=-10;  GmSubMessage(EnemyName+"から逃げ切った","・プレイヤー"+(PlNum+1)+"が"+EnemyName+"から逃走!",PlNum);}
                             else
-                            {
+                            {//相手の攻撃
                                 GmOneMessage("逃亡に失敗しました",PlNum);
                                 GmOneMessage("相手が攻撃を行います",PlNum);
                                 client.getDamage(EnemyStates[Enemy][1]);
@@ -152,7 +169,8 @@ public class ChatServer
                         }
                     }
                 }
-                else if(NextCount==2){
+                else if(NextCount==2)
+                {//休憩開始
                     GmOneMessage("休憩します\n",PlNum);
                     client.EatMeat();
                     GmOneMessage("現在の体力は"+client.getHP()+"です。",PlNum);
@@ -163,23 +181,40 @@ public class ChatServer
             Game(1,Turn+=1);
            }
         }
-        else if(Phase==2){GmAllMessage("\n\n[GM] ゲーム終了です。");}
+        else if(Phase==2)
+        {//結果表示
+            GmAllMessage("\n\n[GM] 誰かが力尽きたため、ゲーム終了となります！");
+            GmAllMessage("[GM] ここから、結果を発表します！");
+
+            int MaxMeat=0;
+            int PlayerNum=0;
+            List<Integer> Used = new ArrayList<>();
+            GmAllMessage("---お肉獲得量---");
+            for(int N=0;N<clients.size();N++)
+            {
+                for(ClientHandler Check : clients){if(MaxMeat>Check.HaveMeat() && !Used.contains(Check.RetPlayer())) {MaxMeat=Check.HaveMeat(); PlayerNum=Check.RetPlayer();}}
+                GmAllMessage( (N+1)+"位   Player["+PlayerNum+"]   "+MaxMeat+"個");
+                Used.add(PlayerNum);
+            }
+            for(ClientHandler client : clients){client.CanMove=true;}
+        }
     }
 
+
+    //全体に向かってアナウンス
     private static void GmAllMessage(String Info)
     {for (ClientHandler client : clients) {client.sendMessage(Info);}}
+    //指定した個人に対してアナウンス
     private static void GmOneMessage(String Info,int PlayerN)
     {
         int This=0;
-        for (ClientHandler client : clients) 
-        {if(This==PlayerN){client.sendMessage(Info);} This++;}
+        for (ClientHandler client : clients) {if(This==PlayerN){client.sendMessage(Info);} This++;}
     }
+    //指定した個人とその他で別のアナウンス内容
     private static void GmSubMessage(String Info,String SubInfo,int PlayerN)
     {
         int This=0;
-        for (ClientHandler client : clients) 
-        {if(This==PlayerN){client.sendMessage(Info);}else{client.sendMessage(SubInfo);} This++;}
-
+        for (ClientHandler client : clients) {if(This==PlayerN){client.sendMessage(Info);}else{client.sendMessage(SubInfo);} This++;}
     }
 
 
@@ -199,13 +234,13 @@ public class ChatServer
         private int SkillA;
         private int SkillB;
         private int HP;
+        private int HPMax=10;
         private int Meat;
+
 
         private PrintWriter out; // クライアントへの出力用
         private BufferedReader in; // クライアントからの入力用
-
-        // コンストラクタ
-        public ClientHandler(Socket socket) {this.clientSocket = socket; this.PlayerNum=PlayerCount; CanMove=false;}
+        public ClientHandler(Socket socket) {this.clientSocket = socket; this.PlayerNum=PlayerCount; CanMove=false;}// コンストラクタ
 
         @Override
         public void run() 
@@ -215,45 +250,54 @@ public class ChatServer
                 // クライアントからの入出力ストリームを作成
                 out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8), true); // 自動フラッシュ有効
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
-
-                // クライアントからのメッセージを受信し、全クライアントにブロードキャスト
                 String inputLine;
-                while ((inputLine = in.readLine()) != null) //プレイヤーからの行動受信
-                { 
+                while ((inputLine = in.readLine()) != null) 
+                { //プレイヤーからの行動受信
                     if(CanMove)
-                    {
+                    {//行動可能状態(自身のターン中であるか)の時に判定
                         try
-                        {
+                        {//各行動
                             String Return = inputLine;
-                            // クライアントからのメッセージを全クライアントに送信
-                            //out.println("Server: " + Return);
-                            if(GamePhase==0&&NextCount==0){
+                            if(GamePhase==0&&NextCount==0)
+                            {//プレイヤーのスキル設定一つ目
                                 int Check=Integer.parseInt(inputLine); 
                                 if(Check>0 && Check<9)
-                                {broadcast(inputLine+"を選択"); SkillA=Check; NextNum(1);}  
+                                {
+                                    broadcast(inputLine+"を選択"); SkillA=Check; 
+                                    NextNum(1);
+                                }  
                                 else{out.println("数値が範囲外です。");}                         
                             }
-                            else if(GamePhase==0&&NextCount==1){
+                            else if(GamePhase==0&&NextCount==1)
+                            {//プレイヤーのスキル設定二つ目
                                 int Check=Integer.parseInt(inputLine); 
                                 if(Check>0 && Check<9 && Check!=SkillA)
                                 {broadcast(inputLine+"を選択"); SkillB=Check; NextNum(2);}  
                                 else{out.println("数値が範囲外です。");}                         
                             }
-                            else if(GamePhase==1&&NextCount==0){
-                                
+                            else if(GamePhase==1&&NextCount==0)
+                            {//戦闘か休憩か選択(お肉が足りない場合は強制戦闘)
                                 int Check=Integer.parseInt(inputLine); 
                                 String Info="休憩";
-                                if(Check==1 || Check==2)
-                                {if(Check==1){Info="調達";} broadcast(Info+"を選択");  NextNum(Check);}  
+                                if(Check==1 || Check==2 )
+                                {
+                                    if(Check==1){Info="調達";} broadcast(Info+"を選択");  
+                                    if(!CanRest()){out.println("...しかしお肉が足りなかった!!!\n強制的に戦闘を行います。"); Check=1;}
+                                    NextNum(Check);
+                                }  
                                 else{out.println("数値が範囲外です。");}   
                             }
-                            else if(GamePhase==1&&NextCount==1){
+                            else if(GamePhase==1&&NextCount==1)
+                            {//戦闘時の行動選択(攻撃か逃亡)
                                 int Check=Integer.parseInt(inputLine); 
                                 if(Check==1 || Check==2){NextNum(Check+10);}  
                                 else{out.println("数値が範囲外です。");}   
                             }
                         }
-                        catch(Exception e){broadcast("[Player "+PlayerNum+"]   " + inputLine);}
+                        catch(Exception e)
+                        {//形式外のコメントは「プレイヤ―のなり切ったセリフとして全体に表示」
+                            broadcast("[Player "+PlayerNum+"]   " + inputLine);
+                        }
                     }
                     else{out.println("[GM] 現在あなたのターンではないです");}
 
@@ -267,7 +311,9 @@ public class ChatServer
             }
         }
 
-        private void broadcast(String message) {
+        //クライアントから全体にメッセージを送る物(OneMessageと統合可能な気がする)
+        private void broadcast(String message) 
+        {
             synchronized (clients) {
                 for (ClientHandler client : clients) 
                 {
@@ -275,9 +321,9 @@ public class ChatServer
                 }
             }
         }
-        private void sendMessage(String message) {
-            out.println(message);
-        }
+        //対象にメッセージを送信
+        private void sendMessage(String message) {out.println(message);}
+        //クライアントハンドル側からサーバーの進行度数値を変更
         private void NextNum(int A)
         {
             synchronized (ChatServer.class) 
@@ -287,10 +333,25 @@ public class ChatServer
             }
         }
         
+
+        //以降ゲーム内数値の取得、変更
+        public int RetPlayer() {return PlayerNum;} 
         public void getMeat(int GMeat){Meat+=GMeat;}
-        public void getDamage(int D){HP-=D;}
-        public void setHP(int H){HP=H;}
+        public int HaveMeat(){return Meat;}
+        public void getDamage(int D){if(D<0){D=0;} HP-=D;}
+        public void setHP(){HP=HPMax;}
         public int getHP(){return HP;}
-        public void EatMeat(){ if(Meat>0){Meat--; HP+=3;}}
+        public boolean CanRest() 
+        {
+            if(SkillA==8||SkillB==8){if(Meat<=1){return false;}else{return true;}}
+            else{if(Meat<=0){return false;}else{return true;}}        
+        }
+        public void EatMeat()
+        { 
+            if(SkillA==8||SkillB==8){if(Meat>1){Meat-=2; HP+=5;if(SkillA==5||SkillB==5){HP+=1;} if(HP>HPMax){HP=HPMax;}}}
+            else{if(Meat>0){Meat--; HP+=3;if(SkillA==5||SkillB==5){HP+=1;} if(HP>HPMax){HP=HPMax;}}}
+        }
+        public int retSkillA() {return SkillA;}
+        public int retSkillB() {return SkillB;}
     }
 }
